bebbcfd7f3047f115191d723d3dde64ac6970852
Atang Fino <atangfmokamogo@outlook.com>
2024-09-23 23:16:50 +0200

    comment spelling

--- fino-atang/src/components/hero-title.jsx
+++ fino-atang/src/components/hero-title.jsx
@@ -1,6 +1,7 @@
 export function heading(){
     // TODO: Split the text sections into different sections with back and front
     // TODO: animate each section.
+    //TODO: listen to mouse event
 
     return (
         <>
