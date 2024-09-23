ca6cd54a261e3aabbd784b12b81a503970016122
Atang Fino <atangfmokamogo@outlook.com>
2024-09-23 22:45:58 +0200

    added a new test section for the split text

---
+++ fino-atang/src/components/hero-title.jsx
@@ -0,0 +1,10 @@
+export function heading(){
+    // TODO: Split the text sections into different sections with back and front
+    // TODO: animate each section.
+
+    return (
+        <>
+            <h1>Hero Title</h1>
+        </>
+    );
+}
